cee610822565cf95340ed54a8efa226a86b2ec4d
Gabriel Waunous <pablogabrielherrera@gmail.com>
2020-04-15 17:38:30 -0300

    Final Module 6

--- app.js
+++ app.js
@@ -1,13 +1,35 @@
+/* eslint-disable linebreak-style */
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require('body-parser');
 
 const app = express();
 
+if (process.env.ENV === 'Test') {
+  console.log('This a Test');
+  const db = mongoose.connect('mongodb://localhost/bookAPI_Test');
+} else {
+  console.log('This a Real');
+  const db = mongoose.connect("mongodb://localhost/bookAPI_prod");
+}
+
+
+
 const port = process.env.PORT || 3000;
+const Book = require('./models/bookModel');
+const bookRouter = require('./routes/bookRouter')(Book);
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
+
+app.use("/api", bookRouter);
 
 app.get("/", (req, res) => {
   res.send("Welcome to my API!");
 });
 
-app.listen(port, () => {
+app.server = app.listen(port, () => {
   console.log(`Running on port ${port}`);
 });
+
+module.exports = app;
